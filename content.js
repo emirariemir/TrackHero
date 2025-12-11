8148980eaa672ce893c3c3d6a066287d28893a25
Emir Arı <emirariomatic@gmail.com>
2025-12-11 20:35:17 +0300

    Add check functionality for already watched videos

--- content.js
+++ content.js
@@ -1,10 +1,10 @@
 // content.js
 
-function waitForElements(selector, callback) {
-  document.querySelectorAll(selector).forEach((el) => callback(el));
+function waitForElements(selector, callback, map) {
+  document.querySelectorAll(selector).forEach((el) => callback(el, map));
 
   const obs = new MutationObserver(() => {
-    document.querySelectorAll(selector).forEach((el) => callback(el));
+    document.querySelectorAll(selector).forEach((el) => callback(el, map));
   });
 
   obs.observe(document.documentElement, { childList: true, subtree: true });
@@ -22,31 +22,54 @@ function getUrlForItem(item) {
   return url;
 }
 
-function addTestButton(item) {
+function buildCompletedMap(playlists) {
+  const map = new Set();
+  playlists.forEach((pl) => {
+    (pl.completedVideos || []).forEach((video) => {
+      map.add(video.id);
+    });
+  });
+  return map;
+}
+
+function addTestButton(item, completedMap) {
   if (item.dataset.trackheroInjected === "true") return;
 
   item.dataset.trackheroInjected = "true";
 
+  const urlForThatVideo = getUrlForItem(item);
+
+  const videoId = urlForThatVideo.searchParams.get("v");
+
+  const isCompleted = completedMap.has(videoId);
+
   const btn = document.createElement("button");
-  btn.textContent = "+";
+
+  if (isCompleted) {
+    btn.textContent = "-";
+    btn.style.background = "#8fffb6";
+    btn.style.color = "#ffffff";
+  } else {
+    btn.textContent = "+";
+    btn.style.background = "#e5e5e5";
+    btn.style.color = "#555";
+  }
+
   btn.style.width = "28px";
   btn.style.height = "28px";
   btn.style.borderRadius = "50%";
   btn.style.display = "flex";
   btn.style.alignItems = "center";
   btn.style.justifyContent = "center";
-  btn.style.background = "#e5e5e5";
   btn.style.border = "none";
-  btn.style.color = "#555";
   btn.style.fontSize = "18px";
   btn.style.fontWeight = "700";
   btn.style.cursor = "pointer";
   btn.style.marginLeft = "8px";
   btn.style.transition = "background 0.2s";
+
   btn.onmouseenter = () => (btn.style.background = "#8fffb6ff");
   btn.onmouseleave = () => (btn.style.background = "#e5e5e5");
-
-  const urlForThatVideo = getUrlForItem(item);
 
   btn.addEventListener("click", () => {
     chrome.runtime.sendMessage({
@@ -58,4 +81,9 @@ function addTestButton(item) {
   item.insertAdjacentElement("beforeend", btn);
 }
 
-waitForElements("#playlist-items", addTestButton);
+chrome.storage.local.get(["playlists"], (result) => {
+  const playlists = result.playlists || [];
+  const completedMap = buildCompletedMap(playlists);
+
+  waitForElements("#playlist-items", addTestButton, completedMap);
+});
